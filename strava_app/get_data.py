117dc75718d6bf48a95da54aec6d245f00c9027b
Matt Dickinson <mattdickinson.j@gmail.com>
2020-11-14 14:18:20 +0000

    adding in strava api get

--- strava_app/get_data.py
+++ strava_app/get_data.py
@@ -1,22 +1,31 @@
 import requests
 import urllib3
+import json
+import os
+
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+refresh_token = os.environ.get('REFRESH_TOKEN')
+
+# import sys, os
+# sys.path.append('/Users/matt/Python/strava-training')
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def get_dataset():
-
     auth_url = "https://www.strava.com/oauth/token"
     activities_url = "https://www.strava.com/api/v3/athlete/activities"
 
     payload = {
-        'client_id': "55362",
-        'client_secret': 'ce6ff8b1b337d045cd05e68e038f716a050d1ef9',
-        'refresh_token': 'aa8407ef4cb66a2ef8c7bc685dc64d9debbd8af4',
+        'client_id': os.environ.get('CLIENT_ID'),
+        'client_secret': os.environ.get('CLIENT_SECRET'),
+        'refresh_token': os.environ.get('REFRESH_TOKEN'),
         'grant_type': "refresh_token",
         'f': 'json'
     }
 
-    # Refresh toen obtained through authorisation code from read_all activities (different to strava default of 'read'). Refresh_token doens't change. See 'activity_readall.txt' for steps
+    # Refresh token obtained through authorisation code from read_all activities (different to strava default of 'read'). Refresh_token doens't change. See 'activity_readall.txt' for steps
     print("Requesting Token...\n")
     res = requests.post(auth_url, data=payload, verify=False)
     # print(res.json())
@@ -25,17 +34,20 @@ def get_dataset():
 
     header = {'Authorization': 'Bearer ' + access_token}
     param = {'per_page': 200, 'page': 1}
-    my_dataset = requests.get(activities_url, headers=header, params=param).json()
+    data = requests.get(activities_url, headers=header, params=param).json()
 
     # print(type(my_dataset)) # List of dicts (name, value pairs)
-    # print(my_dataset)
-    print(my_dataset[1]['name'])
+    # print(json.dumps(data, indent=2))
 
     # Get encoded polyline for use in google maps platform polyline
     # https://developers.google.com/maps/documentation/utilities/polylineutility
 
-    print(my_dataset[1]['map']['summary_polyline'])
+    # print(data[1]['map']['summary_polyline'])
 
-    return my_dataset
+    return data
 
+data = get_dataset()
+print(json.dumps(data[0], indent=2))
+for activity in data:
+    print(activity['name'])
 
--- strava_app/views.py
+++ strava_app/views.py
@@ -1,20 +1,58 @@
 from datetime import datetime
 from flask import Flask, render_template
 from . import app
+import requests
+import urllib3
+import json
+import os
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# from get_data import get_dataset
+
+def get_dataset():
+    auth_url = "https://www.strava.com/oauth/token"
+    activities_url = "https://www.strava.com/api/v3/athlete/activities"
+
+    payload = {
+        'client_id': os.environ.get('CLIENT_ID'),
+        'client_secret': os.environ.get('CLIENT_SECRET'),
+        'refresh_token': os.environ.get('REFRESH_TOKEN'),
+        'grant_type': "refresh_token",
+        'f': 'json'
+    }
+
+    # Refresh token obtained through authorisation code from read_all activities (different to strava default of 'read'). Refresh_token doens't change. See 'activity_readall.txt' for steps
+    print("Requesting Token...\n")
+    res = requests.post(auth_url, data=payload, verify=False)
+    # print(res.json())
+    access_token = res.json()['access_token']
+    print("Access Token = {}\n".format(access_token))
+
+    header = {'Authorization': 'Bearer ' + access_token}
+    param = {'per_page': 200, 'page': 1}
+    # imports as list
+    data = requests.get(activities_url, headers=header, params=param).json()
+
+    return data
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    data = get_dataset()
+    return render_template("home.html", data = data)
 
 @app.route("/about/")
 def about():
     return render_template("about.html")
 
-@app.route("/contact/")
-def contact():
-    return render_template("contact.html")
+@app.route("/api/")
+def api():
+    my_dataset = get_dataset()
 
-@app.route("/api/data")
-def get_data():
-    return app.send_static_file("data.json")
+    return render_template("api.html", data=my_dataset)
+
+# @app.route("/api/data")
+# def get_data():
+
+#     return app.send_static_file("data.json")
